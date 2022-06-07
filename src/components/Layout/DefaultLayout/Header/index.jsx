39d9db1520affe09db83369fed9c61438ae3809e
Huynh Ngoc Tan <18130210@st.hcmuaf.edu.vn>
2022-06-07 14:59:48 +0700

    Xây dựng thanh tìm kiếm trên hearder

--- src/components/Layout/DefaultLayout/Header/index.jsx
+++ src/components/Layout/DefaultLayout/Header/index.jsx
@@ -1,3 +1,9 @@
+import {
+    faMagnifyingGlass,
+    faSpinner,
+    faXmarkCircle,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
 
@@ -5,7 +11,30 @@ const cx = classNames.bind(styles);
 export default function Header() {
     return (
         <div className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <span>Admin</span>
+                </div>
+                <div className={cx('search')}>
+                    <input
+                        className={cx('search-input')}
+                        type="text"
+                        placeholder="Tìm kiếm theo tên người dùng hoặc sản phẩm"
+                    />
+                    <FontAwesomeIcon
+                        className={cx('loading')}
+                        icon={faSpinner}
+                    />
+                    <FontAwesomeIcon
+                        className={cx('clear')}
+                        icon={faXmarkCircle}
+                    />
+                    <button className={cx('search-button')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className="acount-action">Acount</div>
+            </div>
         </div>
     );
 }
