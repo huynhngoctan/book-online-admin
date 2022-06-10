0c0c2ff35f87e68259f98d9f539a9951024a6ebe
Huynh Ngoc Tan <18130210@st.hcmuaf.edu.vn>
2022-06-10 17:35:14 +0700

    Xây dựng sidebar

--- src/components/Layout/DefaultLayout/Sidebar/index.jsx
+++ src/components/Layout/DefaultLayout/Sidebar/index.jsx
@@ -1,12 +1,69 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faBook,
+    faCartShopping,
+    faHouse,
+    faPaperPlane,
+    faUser,
+} from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Sidebar.module.scss';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 export default function Sidebar() {
     return (
         <div className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <h1>Hello</h1>
+                <ul className={cx('list-link')}>
+                    <Link to="/" className={cx('link-route')}>
+                        <li className={cx('link')}>
+                            <FontAwesomeIcon
+                                className={cx('icon')}
+                                icon={faHouse}
+                            />
+                            Trang chủ
+                        </li>
+                    </Link>
+                    <Link to="/users" className={cx('link-route')}>
+                        <li className={cx('link')}>
+                            <FontAwesomeIcon
+                                className={cx('icon')}
+                                icon={faUser}
+                            />
+                            Tài khoản
+                        </li>
+                    </Link>
+
+                    <Link to="/products" className={cx('link-route')}>
+                        <li className={cx('link')}>
+                            <FontAwesomeIcon
+                                className={cx('icon')}
+                                icon={faBook}
+                            />
+                            Sản phẩm
+                        </li>
+                    </Link>
+                    <Link to="/orders" className={cx('link-route')}>
+                        <li className={cx('link')}>
+                            <FontAwesomeIcon
+                                className={cx('icon')}
+                                icon={faCartShopping}
+                            />
+                            Đơn hàng
+                        </li>
+                    </Link>
+                    <Link to="/contact" className={cx('link-route')}>
+                        <li className={cx('link')}>
+                            <FontAwesomeIcon
+                                className={cx('icon')}
+                                icon={faPaperPlane}
+                            />
+                            Liên hệ
+                        </li>
+                    </Link>
+                </ul>
             </div>
         </div>
     );
