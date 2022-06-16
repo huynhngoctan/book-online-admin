24db0c789b3307e510ba442b6dd2d1fad2b72426
Huynh Ngoc Tan <18130210@st.hcmuaf.edu.vn>
2022-06-16 21:28:12 +0700

    Xây dựng button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,56 @@
+import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
+import styles from './Button.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function Button({
+    to,
+    href,
+    primary = false,
+    disabled = false,
+    edited = false,
+    danger = false,
+    onClick,
+    children,
+    leftIcon,
+    rightIcon,
+    className,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = {
+        ...passProps,
+        onClick,
+    };
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof key === 'function') {
+                delete props[key];
+            }
+        });
+    }
+
+    if (href) {
+        Comp = 'a';
+        props.href = href;
+    } else if (to) {
+        Comp = Link;
+        props.to = to;
+    }
+
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        edited,
+        danger,
+        disabled,
+    });
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,21 @@
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from '~/components/Button';
+
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <div>
+            <h1>Home</h1>
+            <Button
+                primary
+                to="https://www.google.com/"
+                leftIcon={<FontAwesomeIcon icon={faHome} />}
+            >
+                Hello
+            </Button>
+            <Button danger to="https://www.google.com/">
+                Hello
+            </Button>
+        </div>
+    );
 }
